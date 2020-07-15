9c383a02d645f07d19fcba4e13ebd4138ca3e287
Jiayi Ren <jiayiren22@gmail.com>
2020-07-14 20:18:22 -0700

    complete doubly linked list

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -27,7 +27,16 @@ class DoublyLinkedList:
     the old head node's previous pointer accordingly.
     """
     def add_to_head(self, value):
-        pass
+        new_node = ListNode(value)
+        # empty list
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.head.prev = new_node
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
         
     """
     Removes the List's current head node, making the
@@ -35,7 +44,22 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_head(self):
-        pass
+        # empty node
+        if self.head is None and self.tail is None:
+            return None
+        # single node in the list
+        if self.head == self.tail:
+            value = self.head.value
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return value
+        # multiple node
+        value = self.head.value
+        self.next.prev = None
+        self.head = self.head.next
+        self.length -= 1
+        return value
             
     """
     Wraps the given value in a ListNode and inserts it 
@@ -43,7 +67,16 @@ class DoublyLinkedList:
     the old tail node's next pointer accordingly.
     """
     def add_to_tail(self, value):
-        pass
+        new_node = ListNode(value)
+        # empty list
+        if self.head is None and self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
             
     """
     Removes the List's current tail node, making the 
@@ -51,32 +84,127 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_tail(self):
-        pass
+        # empty list
+        if self.head is None and self.tail is None:
+            return None
+        # single node in the list
+        if self.head == self.tail:
+            value = self.tail.value
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return value
+        # multiple nodes
+        current = self.head
+
+        while current.next is not self.tail:
+            current = current.next
+        value = self.tail.value
+        self.tail = current
+        self.tail.next = None
+        self.length -= 1
+        return value
             
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List.
     """
     def move_to_front(self, node):
-        pass
+        #  empty node
+        if not node:
+            return None
+        # self is empty
+        if self.head is None and self.tail is None:
+            return None
+        # single node in the list
+        if not self.head.next and not self.tail.prev:
+            return self.head.value
+        # multiple nodes
+        if self.tail == node: # node is the tail
+            self.tail = self.tail.prev
+            self.tail.next = None
+        if self.head == node: # node is the head
+            return self.head.value
+        # node in the middle of list
+        self.head.prev = node
+        node.next = self.head
+        node.prev = None
+        self.head = node
+        value = self.head.value
+        return value
         
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List.
     """
     def move_to_end(self, node):
-        pass
+        # empty node
+        if not node:
+            return None
+        # empty list
+        if self.head is None and self.tail is None:
+            return None
+        # single node
+        if self.head == self.tail:
+            return self.head.value
+        # multiple nodes
+        if self.tail == node: # node is the tail
+            return self.tail.value
+        if self.head == node: # node is the head
+            self.head = self.head.next
+            self.head.prev = None
+        # node in the middle of list
+        self.tail.next = node
+        node.prev = self.tail
+        node.next = None
+        self.tail = node
+        value = self.tail.value
+        return value
 
     """
     Deletes the input node from the List, preserving the 
     order of the other elements of the List.
     """
     def delete(self, node):
-        pass
+        # empty list
+        if self.head is None and self.tail is None:
+            return None
+        # single node in list
+        if not self.head.next and not self.tail.prev:
+            value = node.value
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return value
+        # multiple node in list
+        if node.prev:
+            node.prev.next = node.next
+        else:
+            node.next.prev = None
+            self.head = node.next
+        if node.next:
+            node.next.prev = node.prev
+        else:
+            node.prev.next = None
+            self.tail = node.prev
+        self.length -= 1
+        print(self.length)
+        value = node.value
+        return value
 
     """
     Finds and returns the maximum value of all the nodes 
     in the List.
     """
     def get_max(self):
-        pass
+        # empty list
+        if not self.head:
+            return None
+        # iterate through list and find the max 
+        max_value = self.head.value
+        current = self.head.next
+        while current:
+            if current.value > max_value:
+                max_value = current.value
+            current = current.next
+        return max_value
--- queue/queue.py
+++ queue/queue.py
@@ -15,24 +15,24 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
-# class Queue:
-#     def __init__(self):
-#         self.size = 0
-#         self.storage = []
+class Queue:
+    def __init__(self):
+        self.size = 0
+        self.storage = []
     
-#     def __len__(self):
-#         return self.size
+    def __len__(self):
+        return self.size
 
-#     def enqueue(self, value):
-#         self.storage.append(value)
-#         self.size += 1
+    def enqueue(self, value):
+        self.storage.append(value)
+        self.size += 1
 
-#     def dequeue(self):
-#         if self.size > 0:
-#             self.size -= 1
-#             return self.storage.pop(0)
-#         else:
-#             return None
+    def dequeue(self):
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.pop(0)
+        else:
+            return None
 
 ### Linked List Queue
 class Node:
